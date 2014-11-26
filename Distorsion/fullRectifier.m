f660013f10f4b1deb01f995406215b61e32ac529
Gonzalo Recio <gonya707@gmail.com>
2014-11-26 15:32:34 +0100

    rectifiers
    documented and changed

--- Distorsion/fullRectifier.m
+++ Distorsion/fullRectifier.m
@@ -1,9 +1,15 @@
 function r = fullRectifier(x)
+% function r = fullRectifier(x)
+%
+% Applies a full rectification to the input signal, i.e. Negative values
+%   turn positive and positive values remains the same.
+%
+% Input parameters:
+%   x -> original, un-rectified signal.
+%
+% Output parameters: 
+%   r -> result. Rectified signal.
 
-x = abs(x);
-x = x - mean (x);
-r = normal(x);
-
-
-
+    x = abs(x);
+    r = x - mean (x);
 end
--- Distorsion/halfRectifier.m
+++ Distorsion/halfRectifier.m
@@ -1,8 +1,15 @@
 function r = halfRectifier(x)
+% function r = halfRectifier(x)
+%
+% Applies a half rectification to the input signal, i.e. Negative values
+%   turn to zero while positive values remains the same.
+%
+% Input parameters:
+%   x -> original, un-rectified signal.
+%
+% Output parameters: 
+%   r -> result. Rectified signal.
 
     x(x < 0) = 0;
-    x = x - mean (x);
-    r = normal(x);
-
-
+    r = x - mean (x);
 end
