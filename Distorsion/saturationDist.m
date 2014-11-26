e6e1872310783d8150c1692c262cdc25e72dd133
Gonzalo Recio <gonya707@gmail.com>
2014-11-26 15:24:09 +0100

    zerocrossing and sat distort
    documented and changed

--- Distorsion/saturationDist.m
+++ Distorsion/saturationDist.m
@@ -1,8 +1,18 @@
 function r = saturationDist(x, threshold)
+% function r = saturationDist(x, threshold)
+%
+% Applies saturation distortion to the input signal.
+%
+% Input parameters:
+%   x -> original, un-distorted signal.
+%   threshold -> Threshold value for the distorted section of the signal.
+%       All the result signal values will remain between -threshold and 
+%       threshold.
+%
+% Output parameters: 
+%   r -> result. Distorted signal.
 
     x(x > threshold) = threshold;
     x(x < -threshold) = -threshold;
-    r = normal(x);
-
-
+    r = x;
 end
--- Distorsion/zeroCrossingDist.m
+++ Distorsion/zeroCrossingDist.m
@@ -1,4 +1,16 @@
 function r = zeroCrossingDist(x, threshold)
+% function r = zeroCrossingDist(x, threshold)
+%
+% Applies zero crossing distortion to the input signal.
+%
+% Input parameters:
+%   x -> original, un-distorted signal.
+%   threshold -> Threshold value for the distorted section of the signal.
+%       All the values above -threshold and bellow threshold will be 0 in
+%       the result signal.
+%
+% Output parameters: 
+%   r -> result. Distorted signal.
 
     x(abs(x) < threshold) = 0;
     r = x; 
