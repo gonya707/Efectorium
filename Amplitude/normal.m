1673d992d082f1c57f0424ea56d33c1468bcb25b
Gonzalo Recio <gonya707@gmail.com>
2014-11-26 15:15:33 +0100

    normal.m
    documentation and changes

--- Amplitude/normal.m
+++ Amplitude/normal.m
@@ -1,3 +1,15 @@
 function r = normal(x)
+% function r = normal(x)
+%
+% Normalizes the signal, i.e. adjust the sample values between -1 and 1.
+%
+% Input parameters:
+%   x -> original, un-normalized signal.
+%
+% Output parameters: 
+%   r -> result. Normalized signal.
+%
+% TODO: matrix compatibility
+
     r = x / max(abs(x));
 end
--- demo.m
+++ demo.m
@@ -14,38 +14,3 @@ subplot(4, 2, 1);
 plot(x(left_margin : right_margin));
 title('original');
 axis([1 (right_margin - left_margin) -1 1]);
-
-subplot(4, 2, 2);
-plot(x_dist_zc(left_margin : right_margin));
-title('zero crossing');
-axis([1 (right_margin - left_margin) -1 1]);
-
-subplot(4, 2, 3);
-plot(x_dist_sat(left_margin : right_margin));
-title('saturation');
-axis([1 (right_margin - left_margin) -1 1]);
-
-subplot(4, 2, 4);
-plot(x_dist_exp(left_margin : right_margin));
-title('exponential');
-axis([1 (right_margin - left_margin) -1 1]);
-
-subplot(4, 2, 5);
-plot(x_dist_hr(left_margin : right_margin));
-title('half rectifier');
-axis([1 (right_margin - left_margin) -1 1]);
-
-subplot(4, 2, 6);
-plot(x_dist_fr(left_margin : right_margin));
-title('full rectifier');
-axis([1 (right_margin - left_margin) -1 1]);
-
-subplot(4, 2, 7);
-plot(x_dist_comp(left_margin : right_margin));
-title('comparison');
-axis([1 (right_margin - left_margin) -1 1]);
-
-subplot(4, 2, 8);
-plot(x_dist_ws(left_margin : right_margin));
-title('waveshaping');
-axis([1 (right_margin - left_margin) -1 1]);
