917ea1a017309e77eabc11b01eb5d86cf64d3d67
gonya707 <gonya707@gmail.com>
2014-12-11 21:35:15 +0100

    Testing from DroidEdit. Coded waveshape.m

--- Distorsion/waveShape.m
+++ Distorsion/waveShape.m
@@ -1,12 +1,30 @@
-function r = waveShape(x, input, output)
-%todo input and output as matrixes
+%function r = waveShape(x, ranges, factor)
+	
+	%DEBUG
+	ranges = [[0, 0.2]; [0.2, 0.5]; [-1, -0,5]];
+	factor = [0.5, -0.8, -0.5];
+	
+	
+	
+	%%%%%%%
+	
+	[Ms Ns] = size(ranges);
+	if (Ms == 2 || Ns == 2)
+		if(Ns ~= 2)
+			ranges = ranges';
+		end
+	else
+		error('Malformed ranges matrix. It should be Mx2 or 2xN, being M or N any natural number.');
+	end
+	
+	if (Ms ~= numel(factor))
+		error('Factor matrix lenght must be the same as ranges matrix rows');
+	end
+	
+	r = x;
+	
+	for (i = 1 : Ns)
+		r = r(r > ranges(i,1) && r < ranges(i,2)) = factor(i) * r(r > ranges(i,1) && r < ranges(i,2)); 
+	end
 
-% x_dist_ws (x_dist_ws > 0.5) = -x_dist_ws(x_dist_ws > 0.5);
-% x_dist_ws (x_dist_ws < 0.1) = -x_dist_ws(x_dist_ws < 0.1);
-% x_dist_ws = x_dist_ws - mean (x_dist_ws);
-% x_dist_ws = normal(x_dist_ws);
-
-% debug
-r = 0;
-
-end
+%end
