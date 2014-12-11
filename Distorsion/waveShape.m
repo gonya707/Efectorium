fa71df30b9d59660152e40e13d2673a169254599
gonya707 <gonya707@gmail.com>
2014-12-11 21:44:47 +0100

    Waveshape.m commented

--- Distorsion/waveShape.m
+++ Distorsion/waveShape.m
@@ -1,4 +1,15 @@
 %function r = waveShape(x, ranges, factor)
+% function r = waveShape(x, ranges, factor)
+%
+% Transform some sections of the input wave by mutiplying factors to the original values
+%
+% Input parameters:
+%   x -> original, unmodified signal.
+%   ranges -> specifies the sections of the wave being altered. For example a ranges matrix like [0.1 0.4] will change only x values between 0.1 and 0.4, and all the other values will remain unaltered. Succesive rows of ranges will need aditional values on the factor matrix.
+%		 factor -> factor of change on the values specified on the ranges matrix. The factor matrix must have the sae length as the number of ranges rows. 
+%
+% Output parameters: 
+%   r -> result. Modified signal.
 	
 	%DEBUG
 	ranges = [[0, 0.2]; [0.2, 0.5]; [-1, -0,5]];
