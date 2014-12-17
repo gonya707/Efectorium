19f503b56eb89c9f99e16b9d6832c25ec10738bf
Gonzalo Recio <gonya707@gmail.com>
2014-12-17 21:27:51 +0100

    function filled

--- Noise/addNoise.m
+++ Noise/addNoise.m
@@ -1,12 +1,15 @@
 function r = addNoise(x, w)
 % function r = addNoise(x, w)
 %
-% 
+%   Adds white gaussian noise to the signal.
 %
 % Input parameters:
 %   x -> the original signal
-%   w -> 
+%   w -> Noise decibel level [dBW]
 % Output parameters
 %   r -> result.
+
+L = length(x);
+r = x + wgn(1, L, w);
 
 end
