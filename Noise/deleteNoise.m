4515dfa441bed5c40df40ad6fa3c1db748fb2eb0
Gonzalo Recio <gonya707@gmail.com>
2014-12-19 19:40:41 +0100

    Function done and new audio

--- Noise/deleteNoise.m
+++ Noise/deleteNoise.m
@@ -2,7 +2,7 @@ function r = deleteNoise(x, treshold, window)
 % function r = deleteNoise(x, treshold)
 %
 %   Delete all the signal sections with adjacent samples bellow a given
-%   threshold.
+%   threshold. centerBias() should be used before call this function.
 %
 % Input parameters:
 %   x -> the original signal.
@@ -11,6 +11,18 @@ function r = deleteNoise(x, treshold, window)
 % Output parameters
 %   r -> result.
 
-%
+x = data;
+treshold = 0.03;
+window = 5;
+
+L = length(x);
+r = zeros(L, 1);
+
+% TODO needs optimization
+for i = 1 : L - window
+    if (sum( abs(x(i : i + window)) > treshold) ~= 0)
+        r(i) = x(i);
+    end
+end
 
 end
