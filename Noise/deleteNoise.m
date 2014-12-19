a9f341ceda48808bae1b9fd33e0c489c00f270d9
Gonzalo Recio <gonya707@gmail.com>
2014-12-19 19:14:40 +0100

    Changed prototype

--- Noise/deleteNoise.m
+++ Noise/deleteNoise.m
@@ -1,12 +1,16 @@
-function r = deleteNoise(x, treshold)
+function r = deleteNoise(x, treshold, window)
 % function r = deleteNoise(x, treshold)
 %
-% 
+%   Delete all the signal sections with adjacent samples bellow a given
+%   threshold.
 %
 % Input parameters:
-%   x -> the original signal
-%   treshold -> 
+%   x -> the original signal.
+%   treshold -> the minimal signal value to be considered noise.
+%   window -> the number of adjacent samples of noise.
 % Output parameters
 %   r -> result.
+
+%
 
 end
