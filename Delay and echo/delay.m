72352ce40752698a34088b7c5017fcae56a01cdc
gonya707 <gonya707@gmail.com>
2014-12-24 13:48:23 +0100

    Uncompleted function

--- Delay and echo/delay.m
+++ Delay and echo/delay.m
@@ -5,10 +5,22 @@ function r = delay(x, nTimes, magnitude)
 %
 % Input parameters:
 %   x -> the original signal
-%   nTimes -> 
-%   magnitude -> 
+%   nTimes -> number of delayed replicas
+%   magnitude -> amplitude factor. A magnitude 100 would mean the amplitude of the replica 1 second after is 100 times smaller than the original, being 1 same amplitude as the input signal.
 % Output parameters
-%   r -> result. Modulated signal.
+%   r -> result. This signal will be larger...[...]
 
+	if(1 ~= numel(nTimes))
+			error('nTimes must be a single number.');
+	end
+
+	if(1 ~= numel(magnitude))
+			error('magnitude must be a single number.');
+	end
+	
+	L = length(x);
+	rep = zeros(nTimes, L)
+
+	
 
 end
