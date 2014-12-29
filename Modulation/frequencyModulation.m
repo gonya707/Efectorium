dd41ad851ca9e54d3998530fabc7eb7ddacbaf3e
gonya707 <gonya707@gmail.com>
2014-12-29 15:19:45 +0100

    error management

--- Modulation/frequencyModulation.m
+++ Modulation/frequencyModulation.m
@@ -10,4 +10,13 @@ function r = frequencyModulation(x, fc, carrier)
 % Output parameters
 %   r -> result. Modulated signal.
 
+		if (1 ~= numel(fc))
+				error('Carrier frequency (2nd parameter) must be a single number.');
+		end
+
+		if(carrier ~= 'sine' && carrier ~= 'saw' && carrier ~= 'square' && carrier ~= 'triangle' && carrier ~= 'ramp')
+				error('Carrier (3rd parameter) must be a string containing one of the supported waveforms: sine, square, saw, ramp or triangle.');
+		end
+		
+		
 end
